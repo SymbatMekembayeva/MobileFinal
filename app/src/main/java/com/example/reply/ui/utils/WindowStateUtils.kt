2572bef46987bdae2a0bb69a566b671d722bbd40
Symbat Mekembayeva <120320419+SymbatMekembayeva@users.noreply.github.com>
2023-12-19 00:01:51 +0600

    Update WindowStateUtils.kt

--- app/src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
+++ app/src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.reply.ui.utils
 
@@ -21,9 +6,7 @@ import androidx.window.layout.FoldingFeature
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
 
-/**
- * Information about the posture of the device
- */
+
 sealed interface DevicePosture {
     object NormalPosture : DevicePosture
 
@@ -50,23 +33,17 @@ fun isSeparating(foldFeature: FoldingFeature?): Boolean {
     return foldFeature?.state == FoldingFeature.State.FLAT && foldFeature.isSeparating
 }
 
-/**
- * Different type of navigation supported by app depending on device size and state.
- */
+
 enum class ReplyNavigationType {
     BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
 }
 
-/**
- * Different position of navigation content inside Navigation Rail, Navigation Drawer depending on device size and state.
- */
+
 enum class ReplyNavigationContentPosition {
     TOP, CENTER
 }
 
-/**
- * App Content shown depending on device size and state.
- */
+
 enum class ReplyContentType {
     SINGLE_PANE, DUAL_PANE
 }
